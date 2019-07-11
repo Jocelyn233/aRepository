9d8e00528e5ed312e0838f1f3430bd65da6b2daf
Jocelyn <791245613@qq.com>
2019-07-11 20:37:20 +0800

    各文件创建完成

--- src/index.js
+++ src/index.js
@@ -2,4 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+// 1.引入字体图标
+import "./styles/iconfont.css"
+
 ReactDOM.render(<App />, document.getElementById('root'));
